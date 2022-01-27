ab32118f14d8812f7fda67599ad5e99c4aa842f2
hannajohansen <89612756+hannajohansen@users.noreply.github.com>
2022-01-27 08:35:39 +0100

    php

--- login.php
+++ login.php
@@ -3,23 +3,26 @@
   <head>
     <meta charset="utf-8">
     <link rel="stylesheet" href="css/stilark.css">
-    <title>login</title>
+    <title>logg inn</title>
   </head>
   <body>
     <h1>dette er en loginside</h1>
-      <from action="process.php" method="POST">
+      <form class="" action="process.php" method="post">
         <p>
           <label>Brukernavn</label><br>
-          <input type="text" id="bruker" ="bruker"/>
+          <input type="text" id="bruker" name="bruker"/>
         </p>
+
         <p>
           <label>Passord</label><br>
-          <input type="password" id="pass" ="pass"/>
+          <input type="password" id="passord" name="passord"/>
         </p>
+
         <p>
-          <input type="submit" id="btn" value="Logg inn">
+          <input type="submit" id="btn" value="Logg inn"/>
         </p>
       </form>
+
       <button type="button" name="button">logg inn som gjest</button>
       <p><button type="button" name="Registrerl">Registrer Lærer</button>
       <button type="button" name="Registrers">Registrer Student</button></p>
--- process.php
+++ process.php
@@ -1,18 +1,25 @@
 <?php
-  //get value
+  //verdi
   $brukernavn = $POST['brukernavn']
   $passord = $POST['passord']
 
-  //prevent mysql injection
+  //forhindre mysql-injeksjon
   $brukernavn = stripcslashes($brukernavn);
   $passord = stripcslashes($passord);
   $brukernavn = mysql_real_escape_string($brukernavn);
   $passord = mysql_real_escape_string($passord);
 
-  //connect to the server
+  //koble til server
   mysql_connect("localhost", "root", "");
   mysql_select_db("login");
 
-  //query to the server and select Database
-  $result = mysql_query("select * from users where username = '$username' and password ='$passord'") or die();
+  //spørre til server og velge database
+  $result = mysql_query("velge * fra brukerne hvor brukernavn = '$username' og passord = '$passord'")
+              or die("kunne ikke spørre databasen ".mysql_error());
+  $row = mysql_fetch_array($result);
+  if ($row['brukernavn'] == $brukernavn && $row['passord'] == $passord ) {
+    echo "din innlogging ble suksessfull velkommen".row['brukernavn'];
+  } else {
+    echo "din innlogging ble feil";
+  }
    ?>
