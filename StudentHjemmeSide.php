d6d7a2abf7b983b180624b3f61fa5dfa1bfc8663
Thomas Waaler Hansen <thomawha@hiof.no>
2022-01-30 12:28:07 +0100

    Student hjemmeside

---
+++ StudentHjemmeSide.php
@@ -0,0 +1,107 @@
+<?php
+  include_once 'includes/dbh.inc.php';
+  $_SESSION['user_id'] = 1;
+
+  $stu_sql = "SELECT navn, etternavn, e_post, studiekull, studieretning FROM student, studieretning WHERE student.id=? AND
+  student.studieretning_id = studieretning.id";
+  $stu_stmt = mysqli_stmt_init($conn);
+
+  $emn_sql = "SELECT emne.navn from emne
+  INNER JOIN studieretning_has_emne she ON emne.id = she.emne_id
+  INNER JOIN studieretning sd on she.studieretning_id = sd.id
+  INNER JOIN student st on sd.id = st.studieretning_id
+  WHERE st.id = ?";
+
+  $emn_stmt = mysqli_stmt_init($conn);
+ ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Hjemmeside</title>
+</head>
+<body>
+<header>
+    <div class="nav-bar">
+        <h1>Student hjemmeside</h1>
+        <button>Logg ut</button>
+    </div>
+</header>
+<main class="main">
+    <div class="fag-seksjon">
+        <h2>Liste med fag</h2>
+        <div>
+            <ul>
+              <?php
+              if (!mysqli_stmt_prepare($emn_stmt, $emn_sql)) {
+				            echo "SQL statment failed";
+			      } else {
+                mysqli_stmt_bind_param($emn_stmt, "i", $_SESSION['user_id']);
+                mysqli_stmt_execute($emn_stmt);
+                $emn_result = mysqli_stmt_get_result($emn_stmt);
+                while($emne_row = mysqli_fetch_assoc($emn_result)) {
+                  echo '
+                  <li>',$emne_row["navn"],'</li>
+                  ';
+                }}
+               ?>
+            </ul>
+        </div>
+    </div>
+    <div class="student-info">
+        <h2>Student Informasjon</h2>
+        <table>
+            <tr>
+                <th>Navn</th>
+                <th>Epost</th>
+                <th>Studentretning</th>
+                <th>Studiekull</th>
+            </tr>
+            <tr>
+              <?php
+              if (!mysqli_stmt_prepare($stu_stmt, $stu_sql)) {
+				            echo "SQL statment failed";
+			      } else {
+                mysqli_stmt_bind_param($stu_stmt, "i", $_SESSION['user_id']);
+                mysqli_stmt_execute($stu_stmt);
+                $stu_result = mysqli_stmt_get_result($stu_stmt);
+                while($student_row = mysqli_fetch_assoc($stu_result)) {
+                  echo '
+                  <th>',$student_row["navn"],' ',$student_row["etternavn"],'</th>
+                  <th>',$student_row["e_post"],'</th>
+                  <th>',$student_row["studiekull"],'</th>
+                  <th>',$student_row["studieretning"],'</th>
+                  ';
+                }}
+               ?>
+
+            </tr>
+        </table>
+    </div>
+</main>
+</body>
+</html>
+
+<style>
+    table, th, td {
+        border:1px solid black;
+    }
+    .nav-bar {
+        display: flex;
+        justify-content: space-evenly;
+        align-items: center;
+    }
+    .fag-seksjon {
+        display: flex;
+        flex-direction: column;
+    }
+    .student-info {
+        display: flex;
+        flex-direction: column;
+    }
+    .main {
+        display: flex;
+        justify-content: space-evenly;
+    }
+</style>
